35fdba535fbfd4e97c6158098e01bc11d23f3d3a
albafica.wang <albaficaw@gmail.com>
2020-07-19 10:08:03 +0800

    docker中使用composer

---
+++ demo5/php/index.php
@@ -0,0 +1,13 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+$client = new Predis\Client([
+    'scheme' => 'tcp',
+    'host'   => 'redis',
+    'port'   => 6379,
+]);
+$client->set('foo', 'works!');
+$value = $client->get('foo');
+
+echo $value;
